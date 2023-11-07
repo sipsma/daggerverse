2773acc77236db427e3db76023f4703766cf2a6d
Erik Sipsma <erik@sipsma.dev>
2023-11-06 16:15:15 -0800

    test
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

---
+++ abc/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+)
+
+type Abc struct {}
+
+// example usage: "dagger call container-echo --string-arg yo"
+func (m *Abc) ContainerEcho(stringArg string) *Container {
+	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+}
+
+// example usage: "dagger call grep-dir --directory-arg . --pattern GrepDir"
+func (m *Abc) GrepDir(ctx context.Context, directoryArg *Directory, pattern string) (string, error) {
+	return dag.Container().
+		From("alpine:latest").
+		WithMountedDirectory("/mnt", directoryArg).
+		WithWorkdir("/mnt").
+		WithExec([]string{"grep", "-R", pattern, "."}).
+		Stdout(ctx)
+}
+
